c5b047ba5b748cdf6103a05755950786bf67b197
TanishaParhi <74243195+TanishaParhi@users.noreply.github.com>
2020-12-23 22:19:08 -0800

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,43 @@
+class Box {
+    constructor (x,y,width,height) {
+        var options = {
+            restitution:0.4,
+            friction:1,
+            //isStatic:true
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        this.x = x;
+        this.y = y;
+        this.visiblity =255;
+        World.add(world, this.body);
+    }
+
+display() {
+    var pos = this.body.position;
+    var angle = this.body.angle;
+    if (this.body.speed<8) {
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        rect(0,0, this.width, this.height)
+        pop ();
+    } else {
+        push ();
+            World.remove(world,this.body);
+            this.visiblity = this.visiblity-5;
+            tint(255,this.visiblity);
+        pop();
+        
+    }
+
+}
+
+score(){
+    if(this.visiblity<0 && this.visiblity>-105){
+        score++;
+    }
+}
+}
